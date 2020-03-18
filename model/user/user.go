a9737ebfc73c611391a228c738f87eb037528bf0
Taufik Ardiyan <taufik2884@gmail.com>
2020-03-18 22:51:57 +0700

    modify user model

--- model/user/user.go
+++ model/user/user.go
@@ -1,10 +1,12 @@
 package UserModel
 
 type User struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	Email      string `json:"-"`
+	ID         string `json:"id" db:"id"`
+	Name       string `json:"name" db:"name"`
+	Email      string `json:"email" db:"email"`
+	Phone      string `json:"phone" db:"phone"`
 	NotifToken string `json:"-" db:"notifToken"`
 	LastSeen   string `json:"last_seen" db:"lastSeen"`
 	Status     string `json:"status" db:"status"`
+	IsActive   int64  `json:"is_active" db:"isActive"`
 }
